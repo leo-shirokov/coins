ca482e706bc4e94778294f2c4a444de560a49402
Leonid Shirokov <leo@shirokov.nl>
2023-01-27 11:36:08 +0100

    fixed bugs in card descriptions

--- coins.js
+++ coins.js
@@ -12,9 +12,7 @@ export const coins = [
         the first king of the Netherlands William I. Coin denomination: 3 guilders. 
         Diameter: 40 mm (Crown-Size). William I, Prince of Orange-Nassau ruled the
         country from 1815 to 1840.`,
-        info: `William I (Dutch: Willem Frederik), Prince of Orange-Nassau 
-        [24 August 1772 – 12 December 1843] was a Prince of Orange, the King of 
-        the Netherlands and Grand Duke of Luxembourg. King William I's 
+        info: `William I (Dutch: Willem Frederik), 24 August 1772 – 12 December 1843. King William I's 
         parents were the last stadtholder William V, Prince of Orange of the Dutch 
         Republic, and his wife Wilhelmina of Prussia. Feeling threatened by Napoleon, who
         had escaped from Elba, William proclaimed the Netherlands a kingdom on 16 March 1815 at 
@@ -31,7 +29,7 @@ export const coins = [
         coin: 'coin replica',
         description:
             'The obverse of the coin depicts the bust of William II facing right. On the reverse of the coin, between the designation of value, the crowned national coat of arms is depicted. William II was the son of William I and Wilhelmine of Prussia. William II was married to Anna Pavlovna of Russia. He ruled the country from 1840 to 1849.',
-        info: "William II (Dutch: Willem Frederik George Lodewijk`). William II [6 December 1792 – 17 March 1849] was King of the Netherlands, Grand Duke of Luxembourg, and Duke of Limburg. When William II's father (William I), who up to that time ruled as sovereign prince, proclaimed himself king in 1815, he became Prince of Orange as heir apparent of the Kingdom of the Netherlands. With the abdication of his father on 7 October 1840, William II became king. During his reign, the Netherlands became a parliamentary democracy with the new constitution of 1848.",
+        info: "William II (Dutch: Willem Frederik George Lodewijk), 6 December 1792 – 17 March 1849. When he's father (William I), who up to that time ruled as sovereign prince, proclaimed himself king in 1815, he became Prince of Orange as heir apparent of the Kingdom of the Netherlands. With the abdication of his father on 7 October 1840, William II became king. During his reign, the Netherlands became a parliamentary democracy with the new constitution of 1848.",
     },
     {
         id: 'willem3',
@@ -44,7 +42,7 @@ export const coins = [
         coin: 'original coin',
         description:
             'The 1/2 guilder coin (The Halve Gulden, 50 guilder cent) has not been in circulation since 1930. The Halve Gulden, however, is part of the original nominal system adopted on September 18, 1816 in the new currency law. The issue from 1818 is the very first Halve Gulden from the Kingdom of the Netherlands.',
-        info: "William III (Dutch: Willem Alexander Paul Frederik Lodewijk). William III [19 February 1817 – 23 November 1890] was King of the Netherlands and Grand Duke of Luxembourg from 1849 until his death in 1890. He was also the Duke of Limburg from 1849 until the abolition of the duchy in 1866. William III married his cousin Sophie of Württemberg in 1839 and they had three sons. After Sophie's death in 1877 he married Emma of Waldeck and Pyrmont in 1879 and they had one daughter Wilhelmina, who succeeded William to the Dutch throne. ",
+        info: "William III (Dutch: Willem Alexander Paul Frederik Lodewijk). William III, 19 February 1817 – 23 November 1890, was King from 1849 until his death in 1890. William III married his cousin Sophie of Württemberg in 1839 and they had three sons. After Sophie's death in 1877 he married Emma of Waldeck and Pyrmont in 1879 and they had one daughter Wilhelmina, who succeeded William to the Dutch throne. ",
     },
     {
         id: 'wilhelmina',
@@ -57,7 +55,7 @@ export const coins = [
         coin: 'official reissue of the coin',
         description:
             'These are the very first golden ten guilders of Queen Wilhelmina, on which she is depicted as a little girl of ten years. Wilhelmina is depicted on the coin as the reigning monarch, while her mother ruled the country as regent. Wilhelmina was Queen of the Netherlands from 1890 until her abdication in 1948.',
-        info: "Wilhelmina Helena Pauline Maria [31 August 1880 – 28 November 1962] reigned for nearly 58 years, longer than any other Dutch monarch. Her reign saw World War I, the Dutch economic crisis of 1933 and World War II. Wilhelmina ascended to the throne at the age of 10 after her father's death in 1890, under her mother's regency. On 7 February 1901 in The Hague, Queen Wilhelmina married Duke Henry of Mecklenburg-Schwerin. The birth of Princess Juliana, on 30 April 1909, was met with great relief after eight years of her childless marriage.",
+        info: "Wilhelmina Helena Pauline Maria [31 August 1880 – 28 November 1962] reigned for nearly 58 years, longer than any other Dutch monarch. Her reign saw World War I, the Dutch economic crisis of 1933 and World War II. Wilhelmina ascended to the throne at the age of 10 after her father's death in 1890, under her mother's regency. On 7 February 1901 in The Hague, Queen Wilhelmina married Duke Henry of Mecklenburg-Schwerin.",
     },
     {
         id: 'juliana',
@@ -70,7 +68,7 @@ export const coins = [
         coin: 'original coin',
         description:
             'Silver guilders with a portrait of Queen Juliana were first issued in 1954, 6 years after her accession to the throne. Prior to that, her mother, Queen Wilhelmina, was depicted on coins for 62 years. Silver guilders were minted for 13 years during the reign of Queen Juliana until 1967. Jiliana was Queen of the Netherlands from 1948 until her abdication in 1980.',
-        info: 'Juliana Louise Emma Marie Wilhelmina [30 April 1909 – 20 March 2004] was the only child of Queen Wilhelmina and Prince Henry of Mecklenburg-Schwerin. In 1937, she married Prince Bernhard of Lippe-Biesterfeld with whom she had four daughters: Beatrix, Irene, Margriet, and Christina. During the German invasion of the Netherlands in the Second World War, the royal family was evacuated to the United Kingdom. Juliana then relocated to Canada with her children, while Wilhelmina and Bernhard remained in Britain.',
+        info: 'Juliana Louise Emma Marie Wilhelmina [30 April 1909 – 20 March 2004] was the only child of Queen Wilhelmina and Prince Henry of Mecklenburg-Schwerin. In 1937, she married Prince Bernhard of Lippe-Biesterfeld with whom she had four daughters: Beatrix, Irene, Margriet, and Christina. During the German invasion of the Netherlands in the Second World War, the royal family was evacuated to the United Kingdom.',
     },
     {
         id: 'beatrix',
@@ -83,7 +81,7 @@ export const coins = [
         coin: 'original coin',
         description:
             'This is the most recent guilder issued in the Kingdom of the Netherlands and therefore the last guilder to feature a Dutch monarch. 183 years after the issuance of the very first royal guilder in 1812, guilder money in the Netherlands is coming to an end. Beatrix is a member of the Dutch royal house who reigned as Queen of the Netherlands from 1980 until her abdication in 2013.',
-        info: "Beatrix Wilhelmina Armgard born 31 January 1938. In 1966, Beatrix married Claus von Amsberg, a German diplomat, with whom she had three children. When her mother abdicated on 30 April 1980, Beatrix succeeded her as queen. On Koninginnedag (Queen's Day), 30 April 2013, Beatrix abdicated in favour of her eldest son, Willem-Alexander. At the time of her abdication at age 75, Beatrix was the oldest reigning monarch in the country's history",
+        info: "Beatrix Wilhelmina Armgard born 31 January 1938. In 1966, Beatrix married Claus von Amsberg, a German diplomat, with whom she had three children. When her mother abdicated on 30 April 1980, Beatrix succeeded her as queen. On Koninginnedag (Queen's Day), 30 April 2013, Beatrix abdicated in favour of her eldest son, Willem-Alexander. At the time of her abdication at age 75, Beatrix was the oldest reigning monarch in the country's history.",
     },
     {
         id: 'willemalexander',
@@ -96,7 +94,7 @@ export const coins = [
         coin: 'original coin',
         description:
             "At the beginning of 2014, the first Dutch euro coins with a portrait of King Willem-Alexander were published. The design of the coin was presented on October 31, 2013. This coin is from the popular annual set with all 8 euro coins from 2015. Willem-Alexander is King of the Netherlands, having acceded to the throne following his mother's abdication in 2013.",
-        info: "Willem-Alexander Claus George Ferdinand born 27 April 1967. He became Prince of Orange as heir apparent upon his mother's accession as Queen on 30 April 1980, and succeeded her following her abdication on 30 April 2013. He married Máxima Zorreguieta Cerruti in 2002 and they have three daughters: Catharina-Amalia, Princess of Orange, Princess Alexia, and Princess Ariane. At his accession at age 46, Willem-Alexander was Europe's youngest monarch. He is also the first male monarch of the Netherlands since the death of his great-great-grandfather William III in 1890.",
+        info: "Willem-Alexander Claus George Ferdinand born 27 April 1967. He became Prince of Orange as heir apparent upon his mother's accession as Queen on 30 April 1980, and succeeded her following her abdication on 30 April 2013. He married Máxima Zorreguieta Cerruti in 2002 and they have three daughters: Catharina-Amalia, Princess of Orange, Princess Alexia, and Princess Ariane. At his accession at age 46, Willem-Alexander was Europe's youngest monarch.",
     },
     {
         id: 'maxima',
