59d768ae4a865b29bd48d5943d41027411cadf90
Leonid Shirokov <leo@shirokov.nl>
2023-02-01 20:21:47 +0100

    added ru locale

---
+++ ru/Coin.js
@@ -0,0 +1,40 @@
+function Coin(coin) {
+    return `
+    <section class="content__coin">
+        <div class="flip-wrapper">
+            <div class="flip">
+                <div class="flip-face front">
+                    <img src="${coin.image.obverse}">
+                </div>
+                <div class="flip-face back">
+                    <img src="${coin.image.reverse}">
+                </div>
+            </div>
+        </div>
+        <div class="content__coin-description">
+            <div class="card">
+                <div class="card_front">
+                    <div class="coin-header-container">
+                        <div class="coin-description-header">
+                            <p id="${coin.id}">${coin.header}</p>
+                            <p>${coin.king}</p>
+                        </div>
+                        <div class="coin-description-original">
+                        <p>${coin.coin}</p>
+                        </div>
+                    </div>
+                    <div class="coin-description-container">
+                        <p>${coin.description}</p>
+                    </div>
+                </div>
+                <div class="card_back">
+                    <div class="coin-info-container">
+                        <p>${coin.info}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+`;
+}
+export default Coin;
---
+++ ru/script.js
@@ -0,0 +1,42 @@
+import { coins } from './coins.js';
+import Coin from './Coin.js';
+
+function main() {
+    document
+        .querySelector('main.content')
+        .insertAdjacentHTML('beforeend', coins.map(Coin).join(''));
+
+    document.querySelectorAll('.flip-wrapper').forEach((wrapper) => {
+        wrapper.addEventListener('click', () => {
+            wrapper.querySelector('.flip').classList.toggle('rotated');
+        });
+    });
+
+    document.querySelectorAll('.card').forEach((card) => {
+        card.addEventListener('click', () => {
+            card.classList.toggle('flipped');
+        });
+    });
+}
+
+function addUpButton() {
+    const button = document.createElement('div');
+    button.classList.add('upbtn');
+    document.body.append(button);
+    document.addEventListener('scroll', () => {
+        if (window.scrollY > window.innerHeight - 100) {
+            button.style.bottom = '45px';
+        } else {
+            button.style.bottom = '-80px';
+        }
+    });
+    button.addEventListener('click', () => {
+        window.scroll({
+            top: 0,
+            behavior: 'smooth',
+        });
+    });
+}
+
+main();
+addUpButton();
