82ed59c01285680052519465e5249746bc657f25
Leonid Shirokov <leo@shirokov.nl>
2023-02-02 20:56:57 +0100

    added nl locale

---
+++ nl/coins.js
@@ -0,0 +1,117 @@
+export const coins = [
+    {
+        id: 'willem1',
+        image: {
+            obverse: '../img/willem-1_1.png',
+            reverse: '../img/willem-1_2.png',
+        },
+        header: 'Allereerste gulden, 1818',
+        king: 'Koning Willem I',
+        coin: 'kopie van de munt',
+        description: `De allereerste munt van het Koninkrijk der Nederlanden ooit geslagen met een portret van de eerste koning der Nederlanden, Willem I. Waarde van de munt: 3 gulden. Diameter: 40 mm (kroonmaat). Willem I, Prins van Oranje-Nassau, regeerde Nederland van 1815 tot 1840.`,
+        info: `Willem I (Willem Frederik), 24 augustus 1772 — 12 december 1843. De ouders van koning Willem I waren de laatste stadhouder van de Republiek der Verenigde Provinciën, Willem V, prins van Oranje en zijn vrouw Wilhelmina van Pruisen. Zich bedreigd voelend door Napoleon, die het Elba was ontvlucht, riep Willem V op 16 maart 1815, op aandringen van de op het Congres van Wenen verzamelde machten, Nederland uit tot koninkrijk.`,
+    },
+    {
+        id: 'willem2',
+        image: {
+            obverse: '../img/willem-2_1.png',
+            reverse: '../img/willem-2_2.png',
+        },
+        header: 'Gouden Vijfje, 1843',
+        king: 'Koning Willem II',
+        coin: 'kopie van de munt',
+        description:
+            'Op de voorzijde van de munt is een borstbeeld van Willem II naar rechts. De tegenzijde van de munt toont het gekroond nationale wapen tussen de waardeaanduiding 5 G. Willem II was de zoon van Willem I en Wilhelmina van Pruisen. Willem II was getrouwd met de zus van de Russische keizer Alexander I, Anna Paulowna. Hij regeerde het land van 1840 tot 1849.',
+        info: 'Wilhelm II (Willem Frederik George Lodewijk), 6 december 1792 — 17 maart 1849 Toen zijn vader, Willem I, zichzelf in 1815 tot koning uitriep, werd hij Prins van Oranje als erfgenaam van het Koninkrijk der Nederlanden. Na de troonsafstand van zijn vader op 7 oktober 1840 werd Willem II koning. Tijdens zijn bewind werd Nederland een parlementaire democratie met een nieuwe grondwet in 1848.',
+    },
+    {
+        id: 'willem3',
+        image: {
+            obverse: '../img/willem-3_1.png',
+            reverse: '../img/willem-3_2.png',
+        },
+        header: 'Halve Gulden, 1860',
+        king: 'Koning Willem III',
+        coin: 'originele munt',
+        description:
+            'De 50 guldencent munt is sinds 1930 niet meer in roulatie geweest. De Halve Gulden maakt echter wel onderdeel uit van het originele nominale stelsel dat op 18 september 1816 wordt aangenomen in de nieuwe muntwet. Onder Willem I, II en III kent de munt een grillig productie verloop met opvallende productie aantallen.',
+        info: "Willem III (Willem Alexander Paul Frederik Lodewijk), 19 februari 1817 — 23 november 1890, was koning van 1849 tot aan zijn dood in 1890. Willem III trouwde in 1839 met zijn nicht Sophia van Württemberg en ze kregen drie zonen. Na Sophia's dood in 1877 trouwde hij in 1879 met Emma van Waldeck-Pyrmont en ze kregen een dochter, Wilhelmina, die Willem III opvolgde op de Nederlandse troon.",
+    },
+    {
+        id: 'wilhelmina',
+        image: {
+            obverse: '../img/wilhelmina_1.png',
+            reverse: '../img/wilhelmina_2.png',
+        },
+        header: 'Allereerste Gouden Tientje, 1892',
+        king: 'Koningin Wilhelmina',
+        coin: 'officiële herslag van de munt',
+        description:
+            'De kopzijde toont het portret van Wilhelmina als meisje met hangend lang haar, kijkend naar links. De tegenzijde toont het gekroonde Nederlandse wapenschild tussen de waardeaanduiding 10 G. Wilhelmina is de achterkleindochter van de Russische keizer Paul I. Zij was koningin der Nederlanden van 1890 tot haar troonsafstand in 1948.',
+        info: 'Wilhelmina (Wilhelmina Helena Pauline Maria), 31 augustus 1880 — 28 november 1962, regeerde bijna 58 jaar, langer dan enige andere Nederlandse monarch. Haar bewind viel op de jaren van de Eerste en Tweede Wereldoorlog, de economische crisis van 1933 in Nederland. Wilhelmina besteeg de troon op 10-jarige leeftijd na de dood van haar vader in 1890. Op 7 februari 1901 trouwde koningin Wilhelmina in Den Haag met hertog Heinrich van Mecklenburg-Schwerin.',
+    },
+    {
+        id: 'juliana',
+        image: {
+            obverse: '../img/juliana_1.png',
+            reverse: '../img/juliana_2.png',
+        },
+        header: 'Laatste Zilveren omloopgulden, 1967',
+        king: 'Koningin Juliana',
+        coin: 'originele munt',
+        description:
+            'De kopzijde toont het portret van Juliana naar rechts. De tegenzijde toont het gekroonde Nederlandse wapenschild tussen de waardeaanduiding. Slechts 13 jaar worden er onder het bewind van Juliana zilveren guldens uitgegeven. In 1967 worden zowel de laatste zilveren als eerste nikkelen omloopguldens uitgegeven. Juliana was koningin der Nederlanden van 1948 tot haar troonsafstand in 1980.',
+        info: 'Juliana (Juliana Louise Emma Marie Wilhelmina), 30 april 1909 — 20 maart 2004, trouwde in 1937 met prins Bernhard van Lippe-Biesterfeld, met wie ze vier dochters kreeg: Beatrix, Irene, Margriet en Christina. Tijdens de Duitse inval in Nederland tijdens de Tweede Wereldoorlog werd de koninklijke familie geëvacueerd naar Groot-Brittannië.',
+    },
+    {
+        id: 'beatrix',
+        image: {
+            obverse: '../img/beatrix_1.png',
+            reverse: '../img/beatrix_2.png',
+        },
+        header: 'Laatste gulden, 2001',
+        king: 'Koningin Beatrix',
+        coin: 'originele munt',
+        description:
+            'Het is de meest recente gulden uitgegeven in het Koninkrijk der Nederlanden en daarmee de laatste gulden met een Nederlandse vorst. 183 jaar nadat in 1812 de allereerste koninklijke gulden werd uitgegeven, komt er een einde aan het gebruik van de gulden als betaalmiddel. Koningin Beatrix regeerde het land van 1980 tot haar troonsafstand in 2013.',
+        info: 'Koningin Beatrix (Beatrix Wilhelmina Armgard) werd geboren op 31 januari 1938. In 1966 trouwde Beatrix met Claus van Amsberg, een Duitse diplomaat, met wie ze drie kinderen kreeg. Toen haar moeder op 30 april 1980 aftrad, volgde Beatrix haar op als koningin. Op Koninginnedag, 30 april 2013, deed Beatrix afstand van de troon ten gunste van haar oudste zoon, Willem-Alexander.',
+    },
+    {
+        id: 'willemalexander',
+        image: {
+            obverse: '../img/willem-alexander_1.png',
+            reverse: '../img/willem-alexander_2.png',
+        },
+        header: '1 euro, 2015',
+        king: 'Koning Willem-Alexander',
+        coin: 'originele munt',
+        description:
+            'Begin 2014 werden de eerste Nederlandse euromunten met een portret van Koning Willem-Alexander uitgegeven. Het ontwerp van de munt is op 31 oktober 2013 gepresenteerd. Deze munt komt uit de populaire set 2015, die alle acht euromunten van verschillende denominaties bevat. Willem-Alexander is de koning van Nederland, die de troon besteeg na de troonsafstand van zijn moeder in 2013.',
+        info: 'Koning Willem-Alexander Claus George Ferdinand werd geboren op 27 april 1967. Hij werd Prins van Oranje en erfgenaam van de troon nadat zijn moeder de troon besteeg op 30 april 1980, en volgde haar op bij haar troonsafstand op 30 april 2013. In 2002 trouwde hij met Máxima Sorreguieta Cerruti. Ze hebben drie dochters: Katarina-Amalia, Prinses van Oranje, Prinses Alexia en Prinses Ariana. Bij zijn troonsbestijging op 46-jarige leeftijd was Willem-Alexander de jongste monarch van Europa.',
+    },
+    {
+        id: 'maxima',
+        image: {
+            obverse: '../img/maxima_1.png',
+            reverse: '../img/maxima_2.png',
+        },
+        header: 'Herdenkingsuitgifte, 2021',
+        king: 'Koningin Maxima',
+        coin: 'origineel',
+        description: `Maxima (Maxima Zorreguieta Cerruti), 17 mei 1971, is de echtgenote van koning Willem-Alexander. Koningin Maxima wordt in 2021 50 jaar. Dit jubileum werd gemarkeerd door de uitgifte van een vergulde herdenkingsmunt ter ere van de verjaardag van de koningin.`,
+        info: `In april 1999 ontmoette Maxima kroonprins Willem-Alexander in de Spaanse stad Sevilla. Ruim een jaar later verhuisde zij naar Brussel, waar zij tot april 2001 bij het EU-kantoor van Deutsche Bank heeft gewerkt. Op 17 mei 2001 werd Máxima bij koninklijk besluit het Nederlanderschap verleend. Met de Nederlandse nationaliteit zou zij door haar huwelijk lid worden van het Koninklijk Huis.`,
+    },
+    {
+        id: 'amalia',
+        image: {
+            obverse: '../img/amalia_1.png',
+            reverse: '../img/amalia_2.png',
+        },
+        header: 'Herdenkingsuitgifte, 2021',
+        king: 'Prinses Catharina-Amalia',
+        coin: 'origineel',
+        description: `In 2021 vierde de kroonprinses van Nederland haar 18e verjaardag. Sinds die dag wordt ze formeel aangeduid als Hare Koninklijke Hoogheid de Prinses van Oranje. Dit jubileum werd gemarkeerd door de uitgifte van een vergulde herdenkingsmunt.`,
+        info: `Catharina-Amalia Beatrix Carmen Victoria (Amalia), Prinses van Oranje, Prinses der Nederlanden, Prinses van Oranje-Nassau is de eerste in lijn van troonsopvolging. Ze werd op 7 december 2003 geboren in Den Haag en is het oudste kind van koning Willem-Alexander en koningin Maxima.`,
+    },
+];
+export default coins;
