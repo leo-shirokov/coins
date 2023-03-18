ad5dff60e1813216b92457f0ac065c2ec70365dc
Leonid Shirokov <shirokoff@me.com>
2023-03-18 19:36:20 +0100

    added a crutch for the appearance of a pop-up

--- js/modal.js
+++ js/modal.js
@@ -2,14 +2,18 @@ const openModalBtn = document.querySelector('#modal_open');
 const modal = document.querySelector('.popup');
 const overlay = document.querySelector('.overlay');
 const closeModalBtn = document.querySelector('.modal-close');
+const style = document.createElement('style');
 
 function openModalAndLockScroll() {
+    // style.innerHTML = '.flip::after { content: none }';
+    // document.head.appendChild(style);
     modal.classList.add('modal-open');
     overlay.classList.add('modal-open');
     document.body.classList.add('scroll-lock');
 }
 
 function closeModalAndReturnScroll() {
+    style.remove();
     modal.classList.remove('modal-open');
     overlay.classList.remove('modal-open');
     document.body.classList.remove('scroll-lock');
